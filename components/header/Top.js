19c7db960cfeb92085e7bc23b5be2e1e306cb14c
chihabeddine <chihabeddine@chihabeddines-MacBook-Pro.local>
2023-04-13 14:59:01 +0100

    dispaly logged user from session

--- components/header/Top.js
+++ components/header/Top.js
@@ -1,23 +1,22 @@
 import styles from "./style.module.scss";
-import Link from "next/link";
 import { MdSecurity } from "react-icons/md";
 import { BsSuitHeart } from "react-icons/bs";
-import { RiAccountPinCircleFill, RiArrowDownFill } from "react-icons/ri";
+import { RiAccountPinCircleLine, RiArrowDropDownFill } from "react-icons/ri";
+import Link from "next/link";
 import { useState } from "react";
 import UserMenu from "./UserMenu";
-export default function Top({country}) {
-  const [loggedin, setLoggedIn] = useState(true);
+import { useSession } from "next-auth/react";
+export default function Top({ country }) {
+  const { data: session } = useSession();
+  const [visible, setVisible] = useState(false);
   return (
     <div className={styles.top}>
       <div className={styles.top__container}>
         <div></div>
         <ul className={styles.top__list}>
           <li className={styles.li}>
-            <img
-              src={country.flag}
-              alt=""
-            />
-            <span> {country.name} / USD</span>
+            <img src={country?.flag} alt="" />
+            <span>{country?.name} / USD</span>
           </li>
           <li className={styles.li}>
             <MdSecurity />
@@ -35,64 +34,32 @@ export default function Top({country}) {
               <span>Whishlist</span>
             </Link>
           </li>
-
-         <li>
-         {loggedin ? (
-            <li>
-              <div className={styles.flex}>
-              <img className={styles.menu__img}
-              src="https://res.cloudinary.com/doodo0tre/image/upload/v1680024410/letsChat/anita-austvika-9nbntSgETqQ-unsplash_ancqku.jpg"
-              alt=""
-            />
-                <span>Chihabelddine</span>
-                <RiArrowDownFill />
-              </div>
-            </li>
-          ) : (
-            <li>
-              <div className={styles.flex}>
-                <RiAccountPinCircleFill />
-                <span>Account</span>
-                <RiArrowDownFill />
-              </div>
-            </li>
-          )}
-          <UserMenu loggedin={loggedin} />
-         </li>
+          <li
+            className={styles.li}
+            onMouseOver={() => setVisible(true)}
+            onMouseLeave={() => setVisible(false)}
+          >
+            {session ? (
+              <li className={styles.li}>
+                <div className={styles.flex}>
+                  <img src={session?.user?.image} alt="" />
+                  <span>{session?.user?.name}</span>
+                  <RiArrowDropDownFill />
+                </div>
+              </li>
+            ) : (
+              <li className={styles.li}>
+                <div className={styles.flex}>
+                  <RiAccountPinCircleLine />
+                  <span>Account</span>
+                  <RiArrowDropDownFill />
+                </div>
+              </li>
+            )}
+            {visible && <UserMenu session={session} />}
+          </li>
         </ul>
       </div>
     </div>
   );
-
-  //     <div className={styles.top}>
-  //       <div className={styles.top__container}>
-  //         <div></div>
-  //         <ul className={styles.top__list}>
-  //           <li>
-  //             <img
-  //               src="https://res.cloudinary.com/doodo0tre/image/upload/v1680024410/letsChat/anita-austvika-9nbntSgETqQ-unsplash_ancqku.jpg"
-  //               alt=""
-  //             />
-  //             <span>Morocco / usd</span>
-  //           </li>
-  //           <li>
-  //             <MdSecurity />
-  //             <span>Buyer proection</span>
-  //           </li>
-  //           <li>
-  //             <span>Customer service</span>
-  //           </li>
-  //           <li>
-  //             <span> Help</span>
-  //           </li>
-
-  //           <li>
-  //           <BsSuitHeart />
-  //             <span>Wish list</span>
-  //           </li>
-
-  //         </ul>
-  //       </div>
-  //     </div>
-  //   );
 }
--- components/header/UserMenu.js
+++ components/header/UserMenu.js
@@ -1,18 +1,44 @@
-import React from 'react'
+import Link from "next/link";
 import styles from "./style.module.scss";
-export default function UserMenu({loggedin}) {
+import { signOut, signIn } from "next-auth/react";
+export default function UserMenu({ session }) {
   return (
     <div className={styles.menu}>
-        <h4>Welcome to shopit</h4>
-        {loggedin ? <div className={styles.flex}>
-        <img className={styles.menu__img}
-              src="https://res.cloudinary.com/doodo0tre/image/upload/v1680024410/letsChat/anita-austvika-9nbntSgETqQ-unsplash_ancqku.jpg"
-              alt=""
-            />
-            <div className={styles.col}>
-                
-            </div>
-        </div>:""}
+      <h4>Welcome to Shoppay !</h4>
+      {session ? (
+        <div className={styles.flex}>
+          <img src={session?.user?.image} alt="" className={styles.menu__img} />
+          <div className={styles.col}>
+            <span>Welcome Back,</span>
+            <h3>{session?.user?.name}</h3>
+            <span onClick={() => signOut()}>Sign out</span>
+          </div>
+        </div>
+      ) : (
+        <div className={styles.flex}>
+          <button className={styles.btn_primary}>Register</button>
+          <button className={styles.btn_outlined} onClick={() => signIn()}>
+            Login
+          </button>
+        </div>
+      )}
+      <ul>
+        <li>
+          <Link href="/profile">Account</Link>
+        </li>
+        <li>
+          <Link href="/profile/orders">My Orders</Link>
+        </li>
+        <li>
+          <Link href="/profile/messages">Message Center</Link>
+        </li>
+        <li>
+          <Link href="/profile/address">Address</Link>
+        </li>
+        <li>
+          <Link href="/profile/whishlist">Whishlist</Link>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
