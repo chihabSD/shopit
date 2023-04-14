58eba41538dfd6078c656b1e56bb9f731e43df64
chihabeddine <chihabeddine@chihabeddines-MacBook-Pro.local>
2023-04-14 11:45:29 +0100

    adding user to db and generate jwt

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
 const nextConfig = {
+  
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
+  },
   reactStrictMode: true,
   swcMinify: true,
   sassOptions: {
--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,20 +1,46 @@
 import { validateEmail } from "../../../utils/valdiation";
 import db from "../../../utils/db";
 import nc from "next-connect";
-
+import User from "@/models/user";
+import bcrypt from "bcrypt";
+import { createActivationToken } from "@/utils/tokens";
 const handler = nc();
 
 handler.post(async (req, res) => {
   await db.connectDb();
   const { name, email, password } = req.body;
   try {
-    if(!name || !email || !password){
-        return res.status(400).json({message: "Please fill in all fields"})
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "Please fill in all fields" });
     }
-    if(!validateEmail(email)){
+    if (!validateEmail(email)) {
+      return res.status(400).json({ message: "Please enter a valid email" });
+    }
 
-        return res.status(400).json({message: "Please enter a valid email"})
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "This user already registered" });
     }
+
+    if (password.length < 6) {
+      return res.status(400).json({ message: "Password must be 6 characters" });
+    }
+    const cryptedPassword = await bcrypt.hash(password, 12);
+
+    const newUser = new User({
+      email,
+      password: cryptedPassword,
+      name,
+    });
+    const addedUser = await newUser.save();
+
+    const activation_token = createActivationToken({
+        id:addedUser._id.toString()
+
+    })
+    return res.status(200).json({ token: activation_token });
+
+
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
