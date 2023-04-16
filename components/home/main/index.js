5f27926a03b93155431a10f48090e5154e1fd3d2
chihabeddine <chihabeddine@chihabeddines-MacBook-Pro.local>
2023-04-16 12:58:35 +0100

    adding menu on the side with icons

--- components/home/main/index.js
+++ components/home/main/index.js
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from "react";
 
-import styles from './styles.module.scss'
-import MainSwiper from './swiper'
-import Offers from './offers'
+import styles from "./styles.module.scss";
+import MainSwiper from "./swiper";
+import Offers from "./offers";
+import Menu from "./Menu";
 export default function Main() {
   return (
     <div className={styles.main}>
-        <div className={styles.header}>header</div>
-        <div className={styles.menu}> menu </div>
+      <div className={styles.header}>header</div>
+      <Menu />
       <MainSwiper />
       <Offers />
-        <div className={styles.user}> user </div>
+      <div className={styles.user}> user </div>
     </div>
-  )
+  );
 }
--- components/home/main/offers.js
+++ components/home/main/offers.js
@@ -2,13 +2,14 @@ import React from 'react'
 import styles from './styles.module.scss'
 import { Swiper, SwiperSlide } from "swiper/react";
 
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 
 
 // import required modules
 import { Pagination, Autoplay, Navigation } from "swiper";
+import { offersAarray } from '@/data/home';
+import Link from 'next/link';
 
 export default function App() {
   return (
@@ -22,15 +23,17 @@ export default function App() {
         modules={[Pagination,  Navigation]}
         className="offers_swipper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        {offersAarray.map(offer => (
+
+        <SwiperSlide>
+            <Link href="">
+            <img src={offer.image} alt="" />
+            </Link>
+            <span>{offer.price}$</span>
+            <span>{offer.discount}%</span>
+        </SwiperSlide>
+        ))}
+   
       </Swiper>
     </div>
   );
